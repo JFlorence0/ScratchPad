290f56b480375c6d4bde7c933fa9c52aff56ff61
jimmyfl0 <jflorence0@gmail.com>
2025-01-20 09:24:17 -0500

    add investment themes to DB +

--- obj/Debug/net9.0/ScratchPad.AssemblyInfo.cs
+++ obj/Debug/net9.0/ScratchPad.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScratchPad")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cfba99295fce7a4dbc5208fc538f3d6377ca177")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d1aae57e3d0bcdbf00a019b73423ded5ddb7091")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScratchPad")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScratchPad")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
